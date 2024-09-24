96845f53cb3f25a5f7eedc3a6752f644a6ec9b0c
FUM <53284113+fuhito516@users.noreply.github.com>
2024-09-24 11:22:42 +0900

    up

---
+++ Asset/Javascript/main-footer.js
@@ -0,0 +1,9 @@
+Vue.component('main-footer', {
+    template: `
+    <v-footer color="#CFD8DC80" class="font-weight-black" app>
+        <v-spacer />
+        <span class="copyright" style="color: #606060;">© 2024 Miya Fuhito</span>
+        <v-spacer />
+    </v-footer>
+    `,
+})
